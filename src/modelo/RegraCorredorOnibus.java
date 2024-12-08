399d4959404adb3c322459cddd0e97e35d47e381
kevin <kevin@DESKTOP-IBL3E8A>
2024-12-08 11:22:33 -0300

    commit_Kevin2

---
+++ src/modelo/RegraCorredorOnibus.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class RegraCorredorOnibus extends RegraMulta {
+    private int horaInicio; // Hora inicial da restrição
+    private int horaFim;    // Hora final da restrição
+    private String logradouro; // Local onde a regra se aplica
+
+    public RegraCorredorOnibus(int horaInicio, int horaFim, String logradouro) {
+        this.horaInicio = horaInicio;
+        this.horaFim = horaFim;
+        this.logradouro = logradouro;
+    }
+
+    @Override
+    public Multa calcularMulta(Ocorrencia ocorrencia) {
+        int nivel = verificaNivelMulta(ocorrencia);
+        return nivel > 0 ? new Multa(ocorrencia.getPlaca(), logradouro, obterDescricaoMulta(), nivel) : null;
+    }
+
+    @Override
+    public int verificaNivelMulta(Ocorrencia ocorrencia) {
+        int hora = ocorrencia.getTipoOcorrencia(); // Hora registrada na ocorrência
+        return (hora >= horaInicio && hora <= horaFim) ? 1 : 0; // Infração leve se dentro do horário
+    }
+
+    @Override
+    public String obterDescricaoMulta() {
+        return "Multa por uso irregular de corredor de ônibus no logradouro: " + logradouro;
+    }
+}
